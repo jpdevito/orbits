0b5e2d49a2ce7dd2f02c1ebacc08fafcac19b4bd
jpdevito <justindevito1@gmail.com>
2021-05-21 21:41:45 -0400

    added basic rockets

--- orbits.js
+++ orbits.js
@@ -5,15 +5,20 @@ var innerWidth;
 var innerHeight;
 var spacing;
 var planets;
+var rockets;
+var rocketCounter;
 
 function start() {  
     innerWidth = window.innerWidth
     innerHeight = window.innerHeight
     spacing = innerHeight / 15 // spacing between planets
-    planets = new Array();
+    planets = []; // array to keep track of planets
+    rockets = {}; // dictionary to keep track of rockets
+    rocketCounter = 0;
 
     area.start();  
-    theSun = new sun(80, 80, "Orange");
+
+    theSun = new sun(80, 80, sunImg);
     new planet("Mercury", 10, 10, "Gray", spacing*2, 0);
     new planet("Venus", 20, 20, "Beige", spacing*3, 0);
     earth = new planet("Earth", 20, 20, "DodgerBlue", spacing*4, 0);
@@ -24,6 +29,14 @@ function start() {
     new planet("Neptune", 30, 30, "CornflowerBlue", spacing*10, 0);
 
     this.interval = setInterval(update, 20);  
+
+    // creates new rocket when spacebar pressed
+    document.addEventListener('keydown', function(e) {
+        if(e.which == 32) {
+            new rocket();
+            console.log(rockets); // TEMPORARY
+        }
+    })      
 }  
 
 // canvas element
@@ -41,18 +54,18 @@ var area = {
 } 
 
 // object for drawing sun
-function sun(width, height, color) {  
+function sun(width, height, img) {  
     this.width = width;  
     this.height = height;  
     this.x = (innerWidth - this.width)/2;
     this.y = (innerHeight - this.height)/2;
+    this.img = img
     ctx = area.context;  
-    ctx.fillStyle = color;  
-    ctx.drawImage(sunImg, this.x, this.y, this.width, this.height);  
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);  
 
     this.update = function(){   
         ctx = area.context;  
-        ctx.drawImage(sunImg, this.x, this.y, this.width, this.height);  
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);  
     }
 }  
 
@@ -63,14 +76,15 @@ function planet(name, width, height, color, radius, angle) {
     this.width = width;  
     this.height = height;  
     this.radius = radius;
+    this.mass = width * height;
     this.angle = angle; // angle counterclockwise from horizontal in radians
-    this.speed = getSpeed(radius); // angular speed of planet in rads / update
+    this.angSpeed = getAngSpeed(radius); // angular speed of planet in rads / update
     ctx = area.context;  
     ctx.fillStyle = color;  
     ctx.fillRect((innerWidth - this.width)/2 + this.radius, (innerHeight - this.height)/2, this.width, this.height);  
 
     this.update = function(){  
-        this.angle += this.speed; 
+        this.angle += this.angSpeed; 
         x = (innerWidth - this.width)/2 + Math.cos(this.angle) * this.radius
         y = (innerHeight - this.height)/2 - Math.sin(this.angle) * this.radius
 
@@ -81,9 +95,41 @@ function planet(name, width, height, color, radius, angle) {
 }  
 
 // gets angular speed from radius
-function getSpeed(r) {
-    c = 400 // constant for adjusting speeds
+function getAngSpeed(r) {
+    c = 400 // multiplier for adjusting speeds
     return c * 1/(r*r) // angular velocity obeys inverse square law
+}
+
+// object for drawing rockets
+function rocket() {
+    rocketCounter++;
+    this.id = rocketCounter;
+    rockets[this.id] = this;
+    this.width = 10;
+    this.height = 10;
+    this.x = (innerWidth - this.width)/2 + Math.cos(earth.angle) * earth.radius
+    this.y = (innerHeight - this.height)/2 - Math.sin(earth.angle) * earth.radius
+    earthVel = earth.angSpeed * earth.radius // magnitude of Earth's velocity in pixels / update
+    this.xVel = -1 * earthVel * Math.sin(earth.angle); // velocity in x direction (right is positive)
+    this.yVel = -1 * earthVel * Math.cos(earth.angle); // velocity in y direction (down is positive)
+    
+    ctx = area.context;  
+    ctx.fillStyle = "White";  
+    ctx.fillRect(this.x, this.y, this.width, this.height);  
+
+    this.update = function() {
+        this.x += this.xVel;
+        this.y += this.yVel;
+
+        // if off canvas, remove from dictionary
+        if(this.x < 0 || this.x > innerWidth || this.y < 0 || this.y > innerHeight) {
+            delete rockets[this.id];
+        }
+        
+        ctx = area.context;  
+        ctx.fillStyle = "White";  
+        ctx.fillRect(this.x, this.y, this.width, this.height);  
+    }
 }
 
 // controlles how the page updates each cycle
@@ -92,5 +138,8 @@ function update() {
     theSun.update()
     for (p of planets) {
         p.update();
+    }
+    for (k in rockets) {
+        rockets[k].update();
     }
 }
