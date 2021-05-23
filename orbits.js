f9a2c738dd2e71fed081795b17e9627ca0f570e2
jpdevito <justindevito1@gmail.com>
2021-05-23 16:35:34 -0400

    added collision mechanics

--- orbits.js
+++ orbits.js
@@ -13,6 +13,7 @@ var G; // gravitational constant (can adjust) in units of pixels^3 * pixelmass ^
 // pixelmass = unit of mass (one square pixel, in this program volume = mass)
 // updates = unit of time
 var usePlanetGrav;
+var useCollisions;
 var innerWidth;
 var innerHeight;
 var spacing;
@@ -34,6 +35,7 @@ function start() {
     // can be changed
     G = 0.02;
     usePlanetGrav = true;
+    useCollisions = true;
     launchVel = 1;
 
     loadImages();
@@ -100,17 +102,19 @@ function planet(name, width, height, img, radius, angle) {
     this.mass = width * height;
     this.angle = angle; // angle counterclockwise from horizontal in radians
     this.angSpeed = getAngSpeed(radius); // angular speed of planet in rads / update
+    this.x = (innerWidth/2) + Math.cos(this.angle) * this.radius;
+    this.y = (innerHeight/2) - Math.sin(this.angle) * this.radius;
     this.img = img
     ctx = area.context;   
     ctx.drawImage(this.img, (innerWidth - this.width)/2 + this.radius, (innerHeight - this.height)/2, this.width, this.height);  
 
     this.update = function(){  
         this.angle += this.angSpeed; 
-        xToDraw = (innerWidth - this.width)/2 + Math.cos(this.angle) * this.radius;
-        yToDraw = (innerHeight - this.height)/2 - Math.sin(this.angle) * this.radius;
+        this.x = (innerWidth/2) + Math.cos(this.angle) * this.radius;
+        this.y = (innerHeight/2) - Math.sin(this.angle) * this.radius;
 
         ctx = area.context;   
-        ctx.drawImage(this.img, xToDraw, yToDraw, this.width, this.height);  
+        ctx.drawImage(this.img, this.x - (this.width/2), this.y - (this.height/2), this.width, this.height);  
     }
 }  
 
@@ -130,10 +134,8 @@ function rocket(launchVel, launchAngle) {
     this.launchVel = launchVel;
     this.launchAngle = launchAngle;
     launchRadius = 30; // radius from Earth that the rockets start from
-    xEarth = (innerWidth/2) + Math.cos(earth.angle) * earth.radius;
-    yEarth = (innerHeight/2) - Math.sin(earth.angle) * earth.radius;
-    this.x = xEarth + Math.cos(launchAngle) * launchRadius;
-    this.y = yEarth + Math.sin(launchAngle) * launchRadius;
+    this.x = earth.x + Math.cos(launchAngle) * launchRadius;
+    this.y = earth.y + Math.sin(launchAngle) * launchRadius;
     earthVel = earth.angSpeed * earth.radius // magnitude of Earth's velocity in pixels / update
     this.xVel = (-1 * earthVel * Math.sin(earth.angle)) + (this.launchVel * Math.cos(this.launchAngle)); // velocity in x direction (right is positive)
     this.yVel = (-1 * earthVel * Math.cos(earth.angle)) + (this.launchVel * Math.sin(this.launchAngle)); // velocity in y direction (down is positive)
@@ -158,10 +160,8 @@ function rocket(launchVel, launchAngle) {
         if(usePlanetGrav) {
             for (p of planets) {
                 // distance to each planet
-                xPlanet = (innerWidth/2) + Math.cos(p.angle) * p.radius;
-                yPlanet = (innerHeight/2) - Math.sin(p.angle) * p.radius;
-                xDist = xPlanet - this.x;
-                yDist = yPlanet - this.y;
+                xDist = p.x - this.x;
+                yDist = p.y - this.y;
                 
                 // accelerations from each planet
                 mult = G * p.mass * Math.pow(Math.pow(xDist, 2) + Math.pow(yDist, 2), -1.5); // multiplier in formula used to find both accelerations
@@ -181,6 +181,20 @@ function rocket(launchVel, launchAngle) {
         // if off canvas, remove from dictionary
         if(this.x < 0 || this.x > innerWidth || this.y < 0 || this.y > innerHeight) {
             delete rockets[this.id];
+        }
+
+        if(useCollisions) {
+            //if colliding with the sun, remove from dictionary
+            if(Math.abs(theSun.x - this.x) < theSun.width/2 && Math.abs(theSun.y - this.y) < theSun.height/2) {
+                delete rockets[this.id];
+            }
+
+            // if colliding with a planet, remove from dictionary
+            for (p of planets) {
+                if(Math.abs(p.x - this.x) < p.width/2 && Math.abs(p.y - this.y) < p.height/2) {
+                    delete rockets[this.id];
+                }
+            }
         }
         
         ctx = area.context;  
